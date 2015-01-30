d8dcc120b35272075fcb537675c84459b0fc018d
Tyler McGinnis <tylermcginnis33@gmail.com>
2015-01-30 08:45:15 -0700

    Clean up

--- DashboardCtrl.js
+++ DashboardCtrl.js
@@ -2,21 +2,21 @@ var app = angular.module('firebaseLogin');
 
 app.controller('DashboardCtrl', function($scope, userReference, thingsReference){
 
-	$scope.profile = userReference;
+  $scope.profile = userReference;
 
-	$scope.things = thingsReference;
+  $scope.things = thingsReference;
 
-	$scope.addThing = function(){
-		$scope.things.$add($scope.thing);
-	}
+  $scope.addThing = function(){
+    $scope.things.$add($scope.thing);
+  }
 
-	$scope.removeThing = function(thing){
-		$scope.things.$remove(thing);
-	}
+  $scope.removeThing = function(thing){
+    $scope.things.$remove(thing);
+  }
 
-	$scope.update = function(){
-		$scope.profile.$save();
-	};
+  $scope.update = function(){
+    $scope.profile.$save();
+  };
 
 
 
